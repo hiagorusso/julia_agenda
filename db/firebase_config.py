628f0ac81bfc3f38f99672f2b51f40501427dcde
“hiagorusso” <hiago_ts@hotmail.com>
2024-12-20 00:23:52 -0300

    ajuste chave

--- db/firebase_config.py
+++ db/firebase_config.py
@@ -2,6 +2,7 @@ import firebase_admin
 import json
 from firebase_admin import credentials, firestore
 import os
+import streamlit as st
 
 # # Verifique se o arquivo existe no caminho especificado
 # caminho_credenciais = "C:/Users/HiagoRusso/PycharmProjects/AppAtelie/db/serviceAccountKey.json"
@@ -37,20 +38,30 @@ import os
 #         cred = credentials.Certificate(service_account_path)
 #         firebase_admin.initialize_app(cred)
 #
-#     return firestore.client()
+# #     return firestore.client()
+# service_account_content = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_CONTENT")
+# if service_account_content:
+#     print("Credenciais carregadas com sucesso.")
+# else:
+#     print("A variável de credenciais não foi encontrada.")
+#
+
 
 def iniciar_firestore():
     """Inicializa o Firestore de forma segura."""
     if not firebase_admin._apps:
         # Obter o conteúdo das credenciais da variável de ambiente
-        service_account_content = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_CONTENT")
+        service_account_content = st.secrets["firebase"]["GOOGLE_APPLICATION_CREDENTIALS_CONTENT"]
         if not service_account_content:
             raise ValueError("A variável GOOGLE_APPLICATION_CREDENTIALS_CONTENT não está configurada.")
 
-        # Carregar as credenciais a partir do conteúdo da variável de ambiente
-        cred = credentials.Certificate(json.loads(service_account_content))
+        # Criar o arquivo temporário para usar as credenciais
+        service_account_path = "serviceAccountKey.json"
+        with open(service_account_path, "w") as f:
+            f.write(service_account_content)
 
         # Inicializar o Firebase
+        cred = credentials.Certificate(service_account_path)
         firebase_admin.initialize_app(cred)
 
     return firestore.client()
